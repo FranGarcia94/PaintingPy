915b93b2fa65fce384360e7ec44e955e4256a3bd
FranGarcia94 <garces1094@gmail.com>
2022-08-27 03:26:46 +0200

    Update paintingpy.py

--- paintingpy.py
+++ paintingpy.py
@@ -20,7 +20,7 @@ def choose_color():
     color = askcolor()
     color_button.config(bg = color[1])
 
-    return color[1]
+    return color
 
 
 def outline_color():
@@ -29,7 +29,7 @@ def outline_color():
     color_outline = askcolor()
     color_outline_button.config(bg = color_outline[1])
 
-    return color_outline[1]
+    return color_outline
 
 
 def paint( event ):
